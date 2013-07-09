9f83b89add5791e60a36401c89a433e3e5159b62
hawu.air <hawu@macair.com>
2013-07-09 23:21:47 +0800

    pointer to member function

--- syntax.cpp
+++ syntax.cpp
@@ -5,65 +5,9 @@
 
 using namespace std;
 
-class A{
-public:
-    A()
-    {
-    	cout << "A::A()" << endl;
-    }
-    
-    virtual void f()
-    {
-        cout << "A::f()" <<endl;
-    }
-    
-    virtual ~A()
-    {
-        cout << "A::~A()" << endl;
-    }
-};
+    // member function pointer
 
-class B1 : public  virtual A // virtual inheritance
-{
-public:
-    B1()
-    {
-    	cout << "B1::B1()" << endl;
-    }
-  
-	virtual ~B1(){
-    	cout << "B1::~B1()" << endl;
-    }
-    
-    void f()
-    {
-        cout << "B1::f()" << endl;
-    }
-    
-};
-
-class B2 : public  virtual A // virtual inheritance
-{
-public:
-    
-    B2()
-    {
-    	cout << "B2::B2()" << endl;
-    }
-    virtual ~B2()
-    {
-        cout << "B2::~B2()" << endl;
-    }
-    
-    void f()
-    {
-    	cout << "B2::f()" << endl;
-    }
-    
-};
-
-
-class C : public B2, public B1 // thanks to both are virtual inheritance to Base class A
+class C
 {
 public:
     C()
@@ -82,10 +26,25 @@ public:
     }
 };
 
-int syntax()
+int n = 0;
+int main()
 {
-    	// test multi inheritance function ambiguous.
-    C c;
+    typedef void (C::*C_mem)();
     
+    C c;
+    C_mem pmem = &C::f;
+    
+    ((&c)->*pmem)();
+    
+    (c.*pmem)();
+    
+	n ++;
+    if( n < 3)
+    {
+    	typedef int (*pfunc)();
+        pfunc ptr = main;
+        cout << "run main: " << n << endl;
+        ptr();
+    }
     return 0;
 }
