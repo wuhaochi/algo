5199cd801608227fd6f6268cab2b3d36d9e5219b
hawu.air <hawu@macair.com>
2013-07-13 08:39:40 +0800

    iteration in order traverse of binary tree. The code could be amazingly short which is out of my expectation!

--- algo.cpp
+++ algo.cpp
@@ -2,31 +2,58 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
-
+#include "stack"
 using namespace std;
 
-void insertion_sort(int* A, int L)
-{
-    for(int i = 1; i < L; ++i)
+
+class TreeNode{
+private:
+    int v;
+    TreeNode* _left, *_right;
+public:
+    TreeNode(int v): v(v){}
+    TreeNode* inorder();
+    TreeNode*& left(){
+        return _left;
+    }
+    TreeNode*& right()
     {
-        while (i > 0 && A[i] < A[i-1]) {
-            swap(A[i], A[i-1]);
-            --i;
+        return _right;
+    }
+    
+    int value(){ return v;}
+};
+
+TreeNode* TreeNode::inorder(){
+	TreeNode* p = this;
+    stack<TreeNode*> s;
+    while(!s.empty() || p)
+    {
+        while(p){
+            s.push(p);
+            p = p->left();            
         }
+        p = s.top();
+        s.pop();
+        cout << p->value() << " ";
+        p = p->right();
     }
 }
 
-void run_insertion_sort()
-{
-	int A[] = {1, 3, 5, 3, 5, 9, 2, 1};
-    int L = sizeof(A)/sizeof(int);
-	insertion_sort(A, L);
-    copy(A, A+L, ostream_iterator<int>(cout, " "));
-}
 
 int main()
 {
-    run_insertion_sort();
+    TreeNode* head = new TreeNode(1);
+    head->left() = new TreeNode(2);
+    head->right() = new TreeNode(3);
+    head->left() ->left() = new TreeNode(4);
+    head->right() -> right() = new TreeNode(5);
+    head->left() -> right() = new TreeNode(6);
+    head->right() -> left() = new TreeNode(7);
+    head->left() ->right() -> left() = new TreeNode(8);
+    head->right() -> left() -> left() = new TreeNode(9);
+    head->right() -> left() -> left() -> left() = new TreeNode(10);
+    head->right() -> left() -> left() -> left() -> right() = new TreeNode(11);
+    head->inorder();
     return 0;
-    
 }
